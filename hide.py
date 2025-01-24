6b59ccb46befaf02cf271184e865feeabeef3b97
HADEON <52324046+h8d13@users.noreply.github.com>
2025-01-24 17:34:21 +0100

    Update hide.py

--- hide.py
+++ hide.py
@@ -1,21 +1,28 @@
-# Pip install keyboard pywin32 
-# (CTRL + H to hide) 
-## Uses a combination of PowerShell scripting + PyWin32 to perform actions on Windows
-
 import keyboard
+# Sets up listener for command
 import subprocess
+# To run powershell directly
+
 import win32gui
 import win32con
+# For truly hiding a window
+
 import time
+# For .1 sleeps to ensure ooo 
+
+# Config
+# $command: ctrl+h
 
 hidden_windows = []
+# Remember what we've hidden
 is_hidden = False
+# Is it hidden
 
 def toggle_windows():
    global hidden_windows, is_hidden
    if not is_hidden:
        subprocess.run(['powershell.exe', '(New-Object -ComObject shell.application).toggleDesktop()'])
-       time.sleep(0.3)
+       time.sleep(0.1)
        def callback(hwnd, _):
            if win32gui.IsWindowVisible(hwnd) and win32gui.GetWindowText(hwnd):
                win32gui.ShowWindow(hwnd, win32con.SW_HIDE)
@@ -28,5 +35,12 @@ def toggle_windows():
        hidden_windows.clear()
        is_hidden = False
 
+def task_view():
+    import pyautogui as pag
+    pag.hotkey('win', 'tab')  # Simulate Windows Key + Tab
+
 keyboard.add_hotkey('ctrl+h', toggle_windows)
+time.sleep(0.1)
+keyboard.add_hotkey('ctrl+h', task_view)
+
 keyboard.wait()
